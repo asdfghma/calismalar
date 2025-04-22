d06db2988b1590d02cbe713e82467d7b6b8b4758
Bilal <asdfghma@hotmail.com>
2025-04-22 14:38:26 +0300

    Sallayıcı Sıralaması (Shaker Sort)

---
+++ Sıralama Algoritmaları (Sorting Algorithms)/shakersort.py
@@ -0,0 +1,29 @@
+def shaker_sort(arr):
+    n = len(arr)
+    start = 0
+    end = n - 1
+    swapped = True
+
+    while swapped:
+        swapped = False
+
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        end -= 1
+
+        for i in range(end - 1, start - 1, -1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+        start += 1
+    return arr
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = shaker_sort(arr)
+print("Sıralanmış dizi:", sorted_arr)
